74e4db5ceb51a66fd0eaa56d73ee1628e1bddcad
Taylor Fausak <taylor@fausak.me>
2015-04-01 15:00:21 -0500

    Add some comparison benchmarks

--- Bench.hs
+++ Bench.hs
@@ -6,17 +6,20 @@ import Flow
 main :: IO ()
 main = defaultMain
     [ bgroup "application"
-        [ bench "apply x f" $ whnf (apply x) f
+        [ bench "f x" $ whnf f x
+        , bench "apply x f" $ whnf (apply x) f
         , bench "x |> f" $ whnf (x |>) f
         , bench "f <| x" $ whnf (<| x) f
         ]
     , bgroup "composition"
-        [ bench "compose f g" $ whnf (compose f) g
+        [ bench "f . g" $ whnf (f .) g
+        , bench "compose f g" $ whnf (compose f) g
         , bench "f .> g" $ whnf (f .>) g
         , bench "g <. f" $ whnf (<. f) g
         ]
     , bgroup "strict application"
-        [ bench "apply' x f" $ whnf (apply' x) f
+        [ bench "seq x (f x)" $ whnf (seq x) (f x)
+        , bench "apply' x f" $ whnf (apply' x) f
         , bench "x !> f" $ whnf (x !>) f
         , bench "f <! x" $ whnf (<! x) f
         ]
