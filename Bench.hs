783435aa1b7aca4050d4a5ee8958e67c177c6086
Taylor Fausak <taylor@fausak.me>
2015-04-01 12:32:04 -0500

    Create functions and operators for function application

--- Bench.hs
+++ Bench.hs
@@ -5,5 +5,15 @@ import Flow
 
 main :: IO ()
 main = defaultMain
-    [
+    [ bgroup "application"
+        [ bench "apply x f" $ whnf (apply x) f
+        , bench "x |> f" $ whnf (x |>) f
+        , bench "f <| x" $ whnf (<| x) f
+        ]
     ]
+
+x :: ()
+x = ()
+
+f :: a -> a
+f = id
--- Flow.hs
+++ Flow.hs
@@ -1,1 +1,26 @@
 module Flow where
+
+-- * Function application
+
+{- |
+    >>> apply False not
+    True
+-}
+apply :: a -> (a -> b) -> b
+apply x f = f x
+
+{- |
+    >>> False |> not
+    True
+-}
+infixl 0 |>
+(|>) :: a -> (a -> b) -> b
+x |> f = apply x f
+
+{- |
+    >>> not <| False
+    True
+-}
+infixr 0 <|
+(<|) :: (a -> b) -> a -> b
+f <| x = apply x f
