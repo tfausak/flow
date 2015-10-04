1a51191069db4ee0fa7e63e433238f2b791371b7
Taylor Fausak <taylor@fausak.me>
2015-10-04 12:01:58 -0500

    Don't use dollar signs in benchmarks

--- FlowBench.hs
+++ FlowBench.hs
@@ -6,22 +6,22 @@ import Flow
 main :: IO ()
 main = defaultMain
     [ bgroup "application"
-        [ bench "f x" $ whnf f x
-        , bench "apply x f" $ whnf (apply x) f
-        , bench "x |> f" $ whnf (x |>) f
-        , bench "f <| x" $ whnf (<| x) f
+        [ bench "f x" (whnf f x)
+        , bench "apply x f" (whnf (apply x) f)
+        , bench "x |> f" (whnf (x |>) f)
+        , bench "f <| x" (whnf (<| x) f)
         ]
     , bgroup "composition"
-        [ bench "f . g" $ whnf (f .) g
-        , bench "compose f g" $ whnf (compose f) g
-        , bench "f .> g" $ whnf (f .>) g
-        , bench "g <. f" $ whnf (<. f) g
+        [ bench "f . g" (whnf (f .) g)
+        , bench "compose f g" (whnf (compose f) g)
+        , bench "f .> g" (whnf (f .>) g)
+        , bench "g <. f" (whnf (<. f) g)
         ]
     , bgroup "strict application"
-        [ bench "seq x (f x)" $ whnf (seq x) (f x)
-        , bench "apply' x f" $ whnf (apply' x) f
-        , bench "x !> f" $ whnf (x !>) f
-        , bench "f <! x" $ whnf (<! x) f
+        [ bench "seq x (f x)" (whnf (seq x) (f x))
+        , bench "apply' x f" (whnf (apply' x) f)
+        , bench "x !> f" (whnf (x !>) f)
+        , bench "f <! x" (whnf (<! x) f)
         ]
     ]
 
